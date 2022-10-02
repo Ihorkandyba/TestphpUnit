24bb34227ac3c5d49948c9cc6578954c6c9ba769
Ihor kandyba <jeo2145@gmail.com>
2022-10-02 15:55:10 +0300

    my test

---
+++ StudentUnitTest.php
@@ -0,0 +1,18 @@
+<?php
+require 'Student.php';
+use PHPUnit\Framework\TestCase;
+class StudentClassTest extends TestCase {
+	private $name="Ihor";
+	private $surname="Kandyba";
+	private $group=408;
+	private $StudentTestInstance;
+	protected function setUp(): void
+    {
+        $this->StudentTestInstance = new Student();
+    }
+	public function testSurname(){
+		$this->StudentTestInstance->set_surname($this->surname);
+		$this->StudentTestInstance->set_name($this->name);
+		$this->assertEquals($this->group,$this->StudentTestInstance->get_surname());
+	}
+}
