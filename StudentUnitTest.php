7645f69e1d8368893da81cc1300c95779604fe20
Ihor kandyba <jeo2145@gmail.com>
2022-10-02 16:19:55 +0300

    add Age Test

--- StudentUnitTest.php
+++ StudentUnitTest.php
@@ -10,9 +10,13 @@ class StudentClassTest extends TestCase {
     {
         $this->StudentTestInstance = new Student();
     }
-	public function testSurname(){
+	public function testAge(){
+		$this->StudentTestInstance->set_group($this->group);
+		$this->assertEquals($this->group,$this->StudentTestInstance->get_group());	
+	}
+	/*public function testSurname(){
 		$this->StudentTestInstance->set_surname($this->surname);
 		$this->StudentTestInstance->set_name($this->name);
 		$this->assertEquals($this->group,$this->StudentTestInstance->get_surname());
-	}
+	}*/
 }
